03558e93197cc229c61e439de3465825a9318a88
Saleem Vellekkat <saleemmohd_v@hotmail.com>
2023-01-24 12:28:17 +0530

    The application

---
+++ city_redis.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import redis
+
+app = Flask(__name__)
+red = redis.Redis(host='localhost', port=6379, db=0, charset="utf-8", decode_responses=True)
+
+@app.route('/')
+def health_check():
+    return jsonify(message='OK')
+
+@app.route('/city', methods=['POST'])
+def add_or_update_city():
+    data = request.get_json()
+    city_name = data['city']
+    population = data['population']
+    red.set(city_name, population)
+    return jsonify({'status': 'success'})
+
+@app.route('/city/<name>', methods=['GET'])
+def get_population(name):
+    popu = red.get(name)
+    if not popu:
+        return jsonify({'error': 'city not found'})
+    return jsonify(name, popu)
+
+if __name__ == '__main__':
+    app.run(debug=True)
